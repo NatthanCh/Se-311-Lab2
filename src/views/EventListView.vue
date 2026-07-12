8b16aea84c4d5323aeaad8785e0f35e6c990d518
Natthan Champathip <natthan@MacBook-Air-khxng-Natthan.local>
2026-07-13 00:38:12 +0700

    15.3

---
+++ src/views/EventListView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue'
+import CategoryOrganizerCard from '@/components/CategoryOrganizerCard.vue'
+import type {Event} from '@/types'
+import {ref, onMounted} from 'vue'
+import axios from 'axios'
+
+const events =ref<Event[] | null>(null)
+
+onMounted(() => {
+  axios
+    .get('[your mock server url]')
+    .then((response) => {
+      console.log(response.data)
+    })
+    .catch((error) =>{
+      console.error('There was an error!', error)
+    })
+})
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <CategoryOrganizerCard v-for="event in events" :key="'cat-' + event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}</style>
