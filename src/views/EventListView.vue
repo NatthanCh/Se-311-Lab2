f92ad2a67507130f6d7dc6187db94eaec4ec7bef
Natthan Champathip <natthan@MacBook-Air-khxng-Natthan.local>
2026-07-19 14:46:02 +0700

    Lab 3 (2.5)

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import CategoryOrganizerCard from '@/components/CategoryOrganizerCard.vue'
-import type {Event} from '@/types'
-import {ref, onMounted,computed,watchEffect} from 'vue'
+import type { Event } from '@/types'
+import { ref, computed, watchEffect } from 'vue'
 import EventService from '@/service/EventService'
 
-const events =ref<Event[] | null>(null)
+const events = ref<Event[] | null>(null)
 
 const props = defineProps({
   page: {
@@ -22,7 +22,7 @@ watchEffect(() => {
       console.log(response.data)
       events.value = response.data
     })
-    .catch((error) =>{
+    .catch((error) => {
       console.error('There was an error!', error)
     })
 })
@@ -31,8 +31,28 @@ watchEffect(() => {
 <template>
   <h1>Events For Good</h1>
   <div class="events">
-    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <CategoryOrganizerCard v-for="event in events" :key="'cat-' + event.id" :event="event" />
+    
+    
+    <div class="pagination">
+
+      <RouterLink
+        :to="{ name: 'event-list-view', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page !== 1"
+      >
+        &#60; Prev Page
+      </RouterLink>
+
+
+      <RouterLink 
+        :to="{ name: 'event-list-view', query: { page: page + 1 } }"
+        rel="next"
+      >
+        Next Page &#62;
+      </RouterLink>
+    </div>
   </div>
 </template>
 
@@ -41,4 +61,22 @@ watchEffect(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
-}</style>
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+  margin-top: 20px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+  font-weight: bold;
+}
+
+.pagination a[rel="next"] {
+  text-align: right;
+}
+</style>
