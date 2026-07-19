369314bcc621b83cfe68df7359aa722c1e93c926
Natthan Champathip <natthan@MacBook-Air-khxng-Natthan.local>
2026-07-19 22:00:09 +0700

    Lab3 (3.3)

--- src/views/event/LayoutView.vue
+++ src/views/event/LayoutView.vue
@@ -24,13 +24,13 @@ onMounted(() => {
    <div v-if="event">
         <h1>{{ event.title }}</h1>
         <nav>
-            <router-link :to="{ name : 'event-detail-view',params:{ id }}"
+            <router-link :to="{ name : 'event-detail-view'}"
             >Details</router-link>
             |
-            <router-link :to="{ name : 'event-register-view',params:{id}}"
+            <router-link :to="{ name : 'event-register-view'}"
             >Register</router-link>
             |
-            <router-link :to="{ name : 'event-edit-view',params:{id}}"
+            <router-link :to="{ name : 'event-edit-view'}"
             >Edit</router-link>
 
         </nav>
