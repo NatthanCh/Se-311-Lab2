03b2b7ded28ba4fde610619a71823f20810f6e73
Natthan Champathip <natthan@MacBook-Air-khxng-Natthan.local>
2026-08-10 15:29:29 +0700

    fix style.css

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { SpeedInsights } from '@vercel/speed-insights/vue'
 import { RouterLink, RouterView } from 'vue-router'
-import { useMessageStore } from './stores/message';
-import { storeToRefs } from 'pinia';
+import { useMessageStore } from './stores/message'
+import { storeToRefs } from 'pinia'
+
 const store = useMessageStore()
-const {message} = storeToRefs(store)
+const { message } = storeToRefs(store)
 </script>
 
 <template>
@@ -17,14 +18,33 @@ const {message} = storeToRefs(store)
       <h1>Deploy with Vercel</h1>
       <div class="wrapper">
         <nav class="py-6">
-          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500":to="{name: 'event-list-view'}">Event</RouterLink> |
-  <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500":to="{ name: 'student-list-view' }">Students</RouterLink> |
-  <RouterLink :to="{ name: 'about' }">About</RouterLink>
+          <RouterLink 
+            class="font-bold text-gray-700" 
+            exact-active-class="text-green-500" 
+            :to="{ name: 'event-list-view' }"
+          >
+            Event
+          </RouterLink> |
+          <RouterLink 
+            class="font-bold text-gray-700" 
+            exact-active-class="text-green-500" 
+            :to="{ name: 'student-list-view' }"
+          >
+            Students
+          </RouterLink> |
+          <RouterLink 
+            class="font-bold text-gray-700" 
+            exact-active-class="text-green-500" 
+            :to="{ name: 'about' }"
+          >
+            About
+          </RouterLink>
         </nav>
       </div>
     </header>
-     
+
     <RouterView />
   </div>
 </template>
+
 <style></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import './assets/main.css'
-
+import 'nprogress/nprogress.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-import 'nprogress/nprogress.css'
+
 const app = createApp(App)
 import { inject } from '@vercel/analytics'
 inject()
